8d7d72f87c3e68030e4beee0c8047c3082b419a7
ferraz@Eclipse <esfd.ferraz@gmail.com>
2015-06-17 01:47:58 +0100

    Aluno: Construtor com imagem. Correção de tamanho da imagem no construtor e na setImageView, através de um método private

--- src/application/Aluno.java
+++ src/application/Aluno.java
@@ -6,12 +6,14 @@ import javafx.scene.image.ImageView;
 
 public class Aluno {
 
+	private int nTurma;
     private String nome;
-    private int nTurma;
-    //private String imageName;
-    private ImageView imgv;                
+    private ImageView imgv;
+    
+    private static int IMG_WITH = 40;		// Constante com largura de imagem
 
 
+    //--------------------------------Construtores
     public Aluno(){
     	
     }
@@ -26,30 +28,52 @@ public class Aluno {
 	public Aluno(int nTurma, String nome, ImageView imgv) {
         this.nTurma = nTurma;
         this.nome = nome;
-        this.imgv = imgv;
+        
+        // Preparação da Imagem
+        
+        this.imgv = setImgvSize(imgv);
     }
-
+	
+	
+	//----------------------------------------getter&Setters
+	
+	
+	// Nº Turma
     public int getNTurma() {
         return nTurma;
     }
-
     public void setNTurma(int nTurma) {
         this.nTurma = nTurma;
     }
 
+    
+    // Nome
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
     
+    
+    
+    // ImageView
     public ImageView getImgv() {
 		return imgv;
 	}
 
 	public void setImgv(ImageView imgv) {
-		this.imgv = imgv;
+		this.imgv = setImgvSize(imgv);
+	}
+	
+	
+	/* Prepara tamanho e qualidade da imagem */
+	public ImageView setImgvSize (ImageView imgv){
+		imgv.setFitWidth(IMG_WITH);		// resizes the image
+        imgv.setPreserveRatio(true);	// preserving the ratio
+        imgv.setSmooth(true);			// higher quality filtering method
+        imgv.setCache(true);			// cached to improve performance
+        
+        return imgv;
 	}
 }
